96b61c1b48e71cef01375ad8fa8d4a7a784aee67
yashaskv9480 <yashu9480@gmail.com>
2023-12-02 00:58:14 +0530

    First commit

---
+++ db/db.sql
@@ -0,0 +1,70 @@
+--Create Tables--
+
+CREATE TABLE user_details (uid SERIAL PRIMARY KEY, name VARCHAR(30), password VARCHAR(30), address VARCHAR(50), mobile VARCHAR(10), email varchar(40));
+
+Insert into user_details(001,'admin','admin123','RVCE','12345678','admin@gmail.com')
+
+CREATE TABLE user_role_management(rid SERIAL,uid INT ,admin_id INT, ROLE VARCHAR(10),FOREIGN KEY (UID) REFERENCES user_details (uid), FOREIGN KEY (admin_id) REFERENCES user_details (uid), primary key (rid, uid));
+
+CREATE TABLE DEVICE (DEVICE_ID VARCHAR(20), LOGITUDE VARCHAR(20), LATITUDE VARCHAR(20), DESCRIPTION VARCHAR(50), PRIMARY KEY (device_id));
+
+CREATE TABLE DEVICE_MANAGEMENT(UID INT, DEVICE_ID VARCHAR(20), ACCESS BOOLEAN, PRIMARY KEY(UID, DEVICE_ID), FOREIGN KEY (UID) REFERENCES user_details (uid), FOREIGN KEY (device_id) REFERENCES DEVICE (DEVICE_ID));
+
+
+CREATE TABLE SENSOR_PARAMETERS(SLAVE_ID VARCHAR(10),DEVICE_ID VARCHAR(20), REG_ADD VARCHAR(10),KEYS VARCHAR(40), MINVALUE INT, MAXVALUE INT, SIUNIT VARCHAR(5), FOREIGN KEY (DEVICE_ID) REFERENCES DEVICE (DEVICE_ID), PRIMARY KEY (DEVICE_ID,REG_ADD,SLAVE_ID));
+
+
+
+CREATE TABLE SENSOR_VALUE (DEVICE_ID VARCHAR(20),SLAVE_ID VARCHAR(10),REG_ADD VARCHAR(10),VALUE FLOAT, U_TIME TIMESTAMP,PRIMARY KEY (DEVICE_ID, SLAVE_ID, REG_ADD, U_TIME),FOREIGN KEY (DEVICE_ID, REG_ADD, SLAVE_ID) REFERENCES SENSOR_PARAMETERS(DEVICE_ID, REG_ADD, SLAVE_ID));
+
+CREATE TABLE todo (
+  todo_id SERIAL PRIMARY KEY,
+  user_name VARCHAR(30),
+  description VARCHAR(255),
+  completed BOOLEAN,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  end_date DATE
+);
+
+ALTER TABLE sensor_value
+ADD COLUMN d_time VARCHAR(20);
+
+Alter table todo add column end_date DATE;
+  
+--Insert into Device 
+
+INSERT INTO DEVICE VALUES ('1014', '190273', '029380', 'Location');
+
+--Insert into SENSOR_PARAMETERS
+
+INSERT INTO SENSOR_PARAMETERS values('3','1014','0','R',0,1000,'Volts');
+INSERT INTO SENSOR_PARAMETERS values('3','1014','2','Y',0,1000,'Volts');
+INSERT INTO SENSOR_PARAMETERS values('3','1014','4','B',0,1000,'Volts');
+INSERT INTO SENSOR_PARAMETERS values('3','1014','56','Frequency',0,1000,'Hz');
+INSERT INTO SENSOR_PARAMETERS values('2','1014','2','pH',0,1000,'Value');
+INSERT INTO SENSOR_PARAMETERS values('2','1014','3','Temprature',0,1000,'°C');
+INSERT INTO SENSOR_PARAMETERS values('7','1014','0','Weight',0,1000,'KG');
+
+--
+SELECT
+  sv.device_id AS "Device ID",
+  MAX(CASE WHEN sp.reg_add = '0' And sv.slave_id = '3' THEN sv.value END) AS "R",
+  MAX(CASE WHEN sp.reg_add = '2' AND sv.slave_id = '3' THEN sv.value END) AS "Y",
+  MAX(CASE WHEN sp.reg_add = '4' And sv.slave_id ='3' THEN sv.value END) AS "B",
+  MAX(CASE WHEN sp.reg_add = '56' AND sv.slave_id ='3' THEN sv.value END) AS "Frequency",
+  MAX(CASE WHEN sp.reg_add = '2' AND sv.slave_id = '2' THEN sv.value END) AS "Ph",
+  MAX(CASE WHEN sp.reg_add = '3' And sv.slave_id = '2' THEN sv.value END) AS "Temperature",
+   MAX(CASE WHEN sp.reg_add = '0' AND sv.slave_id='7' THEN sv.value END) AS "Weight",
+  TO_CHAR(MAX(sv.u_time), 'YYYY-MM-DD HH24:MI') AS "Time"
+FROM
+  sensor_value sv
+JOIN
+  sensor_parameters sp ON sv.device_id = sp.device_id AND sv.slave_id = sp.slave_id AND sv.reg_add = sp.reg_add
+WHERE
+  sv.device_id = '1014'
+GROUP BY
+  sv.device_id, TO_CHAR(sv.u_time, 'YYYY-MM-DD HH24:MI')
+ORDER BY
+  "Time" LIMIT -1;
+
+
---
+++ server.js
@@ -0,0 +1,209 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const port = process.env.PORT || 3500
+const db = require("./db/dbconnect.js")
+const jwt = require('jsonwebtoken')
+
+const secretKey = process.env.AUTH_KEY;
+
+app.use(cors())
+app.use(express.json())
+
+// Middleware to authenticate JWT tokens
+const authenticateJWT = (req, res, next) => {
+    const token = req.header('Authorization');
+    if (!token) return res.sendStatus(401);
+
+    jwt.verify(token, secretKey, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+};
+
+app.post("/api/v1/login", async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const result = await db.query("SELECT uid FROM user_details WHERE email=($1) AND password=($2)", [email, password])
+        const user = result.rows[0];
+        console.log(req.body)
+        if (user) {
+            if (email == "admin@mail.com")
+                result.rows[0].type = "admin"
+            const token = jwt.sign({ id: user.uid, useremail: email }, secretKey);
+            return res.status(200).json({"token" : token, "type": result.rows[0].type, "uid": result.rows[0].uid})
+           
+        
+        }
+    
+        return res.status(404).json({"message":404});
+    } catch (err) {
+        console.log(err.message)
+        return res.status(500).json({ error: err.message });
+    }
+})
+
+app.get('/api/v1/authenticate', authenticateJWT, (req, res) => {
+    res.status(200).json({ message: 'Valid User 👍' });
+});
+
+
+app.get("/api/v1/devices",async (req,res) =>{
+    try{
+        const result = await db.query("Select * from device")
+
+
+        res.status(200).json(result.rows)
+    }
+    catch(err){
+        console.log(err);
+    }
+})
+
+app.post("/api/v1/adddevice",async (req,res) => {
+    try{
+        const { device_id, longitude, latitude, description } = req.body;
+        const values = [device_id, longitude, latitude, description];
+        const query = "INSERT INTO device (device_id, logitude, latitude, description) VALUES ($1, $2, $3, $4)";
+        const result = await db.query(query, values);
+        console.log(res.status(200).json({
+            "message" : "success"
+        }));
+
+    }
+    catch(err){
+        console.log(err)
+        res.send(err.message)
+    }
+})
+
+app.get("/api/v1/sensor_values", async (req, res) => {
+  try {
+    const result = await db.query(`
+    SELECT
+    sv.device_id AS "device_id",
+    MAX(CASE WHEN sp.reg_add = '0' AND sv.slave_id = '3' THEN sv.value END) AS "r",
+    MAX(CASE WHEN sp.reg_add = '2' AND sv.slave_id = '3' THEN sv.value END) AS "y",
+    MAX(CASE WHEN sp.reg_add = '4' AND sv.slave_id = '3' THEN sv.value END) AS "b",
+    MAX(CASE WHEN sp.reg_add = '56' AND sv.slave_id = '3' THEN sv.value END) AS "frequency",
+    MAX(CASE WHEN sp.reg_add = '2' AND sv.slave_id = '2' THEN sv.value END) AS "ph",
+    MAX(CASE WHEN sp.reg_add = '3' AND sv.slave_id = '2' THEN sv.value END) AS "temperature",
+    MAX(CASE WHEN sp.reg_add = '0' AND sv.slave_id = '7' THEN sv.value END) AS "weight",
+    MAX(sv.d_time) AS "dtime"
+    FROM
+        sensor_value sv
+    JOIN
+        sensor_parameters sp ON sv.device_id = sp.device_id AND sv.slave_id = sp.slave_id AND sv.reg_add = sp.reg_add
+    WHERE
+        sv.device_id = '1014'
+    GROUP BY
+        sv.device_id, sv.d_time
+    ORDER BY
+        "dtime" DESC;
+
+    `);
+
+    console.log(result.rows);
+    res.json(result.rows);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/api/v1/dashboard", async(req,res) =>{
+    try{
+        const result = await db.query(` SELECT
+        sv.device_id AS "device_id",
+        MAX(CASE WHEN sp.reg_add = '0' AND sv.slave_id = '3' THEN sv.value END) AS "r",
+        MAX(CASE WHEN sp.reg_add = '2' AND sv.slave_id = '3' THEN sv.value END) AS "y",
+        MAX(CASE WHEN sp.reg_add = '4' AND sv.slave_id = '3' THEN sv.value END) AS "b",
+        MAX(CASE WHEN sp.reg_add = '56' AND sv.slave_id = '3' THEN sv.value END) AS "frequency",
+        MAX(CASE WHEN sp.reg_add = '2' AND sv.slave_id = '2' THEN sv.value END) AS "ph",
+        MAX(CASE WHEN sp.reg_add = '3' AND sv.slave_id = '2' THEN sv.value END) AS "temperature",
+        MAX(CASE WHEN sp.reg_add = '0' AND sv.slave_id = '7' THEN sv.value END) AS "weight",
+        MAX(sv.d_time) AS "dtime"
+        FROM
+            sensor_value sv
+        JOIN
+            sensor_parameters sp ON sv.device_id = sp.device_id AND sv.slave_id = sp.slave_id AND sv.reg_add = sp.reg_add
+        WHERE
+            sv.device_id = '1014'
+        GROUP BY
+            sv.device_id, sv.d_time
+        ORDER BY
+            "dtime" DESC
+        LIMIT 1; 
+    `)
+    res.status(200).json(result.rows)
+    }
+    catch(err){
+        console.log(err.message)
+    }
+})
+
+//create a todo
+app.post("/api/v1/todo", async (req, res) => {
+    try {
+      const { user_name, description,end_date} = req.body;
+      const newTodo = await db.query(
+        "INSERT INTO todo (user_name, description,end_date) VALUES($1, $2,$3) RETURNING *",
+        [user_name, description,end_date]
+      );
+  
+      res.json(newTodo.rows[0]);
+      console.log(req.body)
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
+
+// Update a todo's completion status
+app.put('/api/v1/todo/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { completed } = req.body;
+  
+      const updateTodo = await db.query(
+        'UPDATE todo SET completed = $1 WHERE todo_id = $2 RETURNING *',
+        [completed, id]
+      );
+  
+      res.json(updateTodo.rows[0]);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  
+  
+  //get all todos
+  
+  app.get("/api/v1/todo", async (req, res) => {
+    try {
+      const allTodos = await db.query("SELECT * FROM todo");
+      res.json(allTodos.rows);
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
+  
+  //delete a todo
+  
+  app.delete("/api/v1/todo/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deleteTodo = await db.query("DELETE FROM todo WHERE todo_id = $1", [
+        id
+      ]);
+      res.json("Todo was deleted!");
+    } catch (err) {
+      console.log(err.message);
+    }
+  });
+
+
+app.listen(port,() => {
+    console.log(`Listening on port ${port}`)
+})
